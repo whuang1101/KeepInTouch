f0e47b90474cc6812cf10d088797d8d4e168fd14
Wilson Huang <wilsonhuang11012000@gmail.com>
2023-08-31 21:10:49 -0400

    Finishi implementing friend requests

--- src/components/FriendRequest.jsx
+++ src/components/FriendRequest.jsx
@@ -1,23 +1,46 @@
 import { useEffect, useState } from "react"
 import Icon from '@mdi/react';
-import { mdiAccountPlus } from '@mdi/js';
+import { mdiAccountPlus, mdiCheck, mdiClose, mdiDotsHorizontalCircleOutline} from '@mdi/js';
+
 const FriendRequest = () => {
     const [allUsers, setAllUsers] = useState(null)
-    const data = JSON.parse(localStorage.getItem("userData"))
+    const data = JSON.parse(localStorage.getItem("userData"));
+    const [friendRequests, setFriendRequest] = useState(null) 
+    const [pendingRequests, setPendingRequest] = useState(null)
     useEffect(() => {
-        fetch("http://localhost:3000/users").then(response => {
+        fetch(`http://localhost:3000/users/${data._id}`).then(response => {
             if(response.ok){
                 return response.json();
             }
             else{
                 throw new Error("Network response was not ok")
             }
-        }).then(data => {setAllUsers(data)})},[]);
-    const handleFriendRequest = (newRecipient) => {
+        }).then(data => {setAllUsers(data), console.log(data)})
+        fetch(`http://localhost:3000/friend-request/${data._id}`).then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            else{
+                throw new Error("Network response was not ok")
+            }
+        }).then(data => {setFriendRequest(data)})
+        fetch(`http://localhost:3000/friend-request/pending/${data._id}`).then(response => {
+            if(response.ok){
+                return response.json();
+            }
+            else{
+                throw new Error("Network response was not ok")
+            }
+        }).then(data => {setPendingRequest(data)} )
+    
+    }
+        ,[]);
+    const handleSendRequest = (newRecipient) => {
         const body = {
-            recipient: newRecipient,
+            recipient: newRecipient.email,
             sender: data.email,
         }
+
         fetch("http://localhost:3000/friend-request", {
             method:"POST",
             headers: {
@@ -26,7 +49,14 @@ const FriendRequest = () => {
             body: JSON.stringify(body),
         }).then(response => {
             if (response.ok){
-                console.log("yay")
+                setAllUsers(users => users.filter(item => (item._id !== newRecipient._id && item._id !== data._id)));
+                setPendingRequest([...pendingRequests, {
+                    _id:0,
+                    recipient: {image_url: newRecipient.image_url,
+                    name:newRecipient.name},
+                    status:2
+                }]);
+
             }
             else {
                 throw new Error(response.status);
@@ -34,19 +64,101 @@ const FriendRequest = () => {
         }).catch((err) =>
         console.log(err))
     }
+    const handleAcceptRequest = (sender) => {
+        const newBody = {
+            recipient: data,
+            sender: sender,
+        }
+            fetch("http://localhost:3000/friend-request/accept", {
+            method:"POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(newBody),
+        }).then(response => {
+            if(response.ok){
+                const hello = friendRequests.filter(item => !(item.sender._id === sender._id));
+                setFriendRequest(hello)        
+            }
+            else {
+                throw new Error(response.status);
+            }
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    const handleDeclineRequest = (sender) => {
+        const newBody = {
+            recipient: data,
+            sender: sender,
+        }
+            fetch("http://localhost:3000/friend-request/decline", {
+            method:"POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(newBody),
+        }).then(response => {
+            if(response.ok){
+                const hello = friendRequests.filter(item => !(item.sender._id === sender._id));
+                setFriendRequest(hello);
+            }
+            else {
+                throw new Error(response.status);
+            }
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
     return (<div className="friends">
-        <div className="friend-request">Friend Requests</div>
+        {friendRequests && friendRequests.length !==0 &&
+        <div className="friend-requests">
+            <div className="friend-request-title">Friend Requests</div>
+            {friendRequests.map((item) => (
+                item.status === 2 &&
+                <div className="friend-request" key={item._id}>
+                    <div className="first-half">
+                        <img src={item.sender.image_url} alt={`${item.sender.name} profile-pic`}  className="profile-pic"/>
+                        <div className="friend-request-name"> {item.sender.name}</div>
+                    </div>
+                    <div className="second-half">
+                        <Icon path={mdiCheck} size={1} style={{color:"green"}} onClick={() => {handleAcceptRequest(item.sender)}}/>
+                        <Icon path={mdiClose} size={1} style={{color:"red"}} onClick={() =>{handleDeclineRequest(item.sender)}}/>
+                    </div>
+                </div>
+            ))}
+        </div>
+        }
+        {pendingRequests && pendingRequests.some(item => item.status === 2) &&
+        <div className="pending-requests">
+            <div className="pending-request-title">Pending Request</div>
+            {pendingRequests.map((item) => (
+                item.status ===2  &&
+                <div className="friend-request" key={item._id}>
+                    <div className="first-half">
+                        <img src={item.recipient.image_url} alt={`${item.recipient.name} profile-pic`}  className="profile-pic"/>
+                        <div className="friend-request-name"> {item.recipient.name}</div>
+                    </div>
+                    <div className="second-half">
+                        <Icon path={mdiDotsHorizontalCircleOutline} size={1} />
+                    </div>
+                </div>
+            ))}
+        </div>}
         {allUsers && 
+         allUsers.length !== 0 &&
         <div className="potential-friends">
             <div className="potential-friends-title">Potential Friends</div>
             {allUsers.map((user) => (
                 user.email !== data.email &&
                 <div className="potential-friend" key={user._id}>
                     <div className="first-half">
-                        <img src={`${user.image_url}`} alt={`${user.name} profile pic`} style={{height:"2em", borderRadius:"1em"}}/>
+                        <img src={`${user.image_url}`} alt={`${user.name} profile pic`} className="profile-pic"/>
                         <div className="potential-friend-name">{user.name}</div>
                     </div>
-                    <Icon path={mdiAccountPlus} size={1} onClick={()=> (handleFriendRequest(user.email))} />
+                    <Icon path={mdiAccountPlus} size={1} onClick={()=> (handleSendRequest(user))} />
                 </div>
             ))}
         </div>}
